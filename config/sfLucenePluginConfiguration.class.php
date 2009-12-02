e7b00938a0ebc9b1b3f8b24de2abb78542d929a7
Thomas Rabaix <thomas.rabaix@soleoweb.com>
2009-12-02 11:47:45 +0000

    [sfLucenePlugin] [solr] load config class if required
    git-svn-id: http://svn.symfony-project.com/plugins/sfLucenePlugin/branches/1.2-Solr@24789 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- config/sfLucenePluginConfiguration.class.php
+++ config/sfLucenePluginConfiguration.class.php
@@ -1,21 +1,43 @@
 <?php
 
-
 class sfLucenePluginConfiguration extends sfPluginConfiguration
 {
 
   public function setup()
   {
-
     if ($this->configuration instanceof sfApplicationConfiguration)
     {
       /**
        * Add configuration handlers for search.yml
        */
+      
+      $this->loadConfigHandlerClasses();
+      
       $configCache = $this->configuration->getConfigCache();
       $configCache->registerConfigHandler('config/search.yml', 'sfLuceneProjectConfigHandler');
       $configCache->registerConfigHandler('modules/*/config/search.yml', 'sfLuceneModuleConfigHandler');
     }
     
+  }
+  
+  /**
+   * 
+   * At this point the autoloaded is not yet started
+   * 
+   */
+  public function loadConfigHandlerClasses()
+  {
+    
+    $lib_folder = sfConfig::get('sf_plugins_dir').'/sfLucenePlugin/lib/config';
+    
+    if(!class_exists('sfLuceneProjectConfigHandler', true))
+    {
+      include_once $lib_folder.'/sfLuceneProjectConfigHandler.class.php';
+    }
+    
+    if(!class_exists('sfLuceneModuleConfigHandler', true))
+    {
+      include_once $lib_folder.'/sfLuceneModuleConfigHandler.class.php';
+    }
   }
 }
