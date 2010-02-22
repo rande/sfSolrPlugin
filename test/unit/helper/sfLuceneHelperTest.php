14574d948e6a871833280fa9945eb578791ab8ea
Thomas Rabaix <thomas.rabaix@soleoweb.com>
2010-02-22 22:33:21 +0000

    [sfSolrPlugin] fix sfLuceneHelperTest
    git-svn-id: http://svn.symfony-project.com/plugins/sfSolrPlugin/branches/sf1.2@28199 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/helper/sfLuceneHelperTest.php
+++ test/unit/helper/sfLuceneHelperTest.php
@@ -16,58 +16,9 @@
 
 require dirname(__FILE__) . '/../../bootstrap/unit.php';
 
-$t = new limeade_test(16, limeade_output::get());
+$t = new limeade_test(7, limeade_output::get());
 
 sfLoader::loadHelpers(array('sfLucene'));
-
-// intercepts includes to partials
-function include_partial($partial = null, $params = null, $dump = false)
-{
-  static $sPartial;
-  static $sParams;
-
-  if ($dump)
-  {
-    return array('partial' => $sPartial, 'params' => $sParams);
-  }
-
-  $sPartial = $partial;
-  $sParams = $params;
-}
-
-class Foo
-{
-  public function getInternalPartial()
-  {
-    return 'FooPartial';
-  }
-}
-class Bar
-{
-}
-
-$foo = new Foo;
-$bar = new Bar;
-
-$t->diag('testing partial dependencies');
-
-include_search_result($foo, 'query');
-$values = include_partial(null, null, true);
-$t->is($values['partial'], 'FooPartial', 'include_search_result() selects the correct partial');
-$t->ok($values['params']['result'] === $foo, 'include_search_result() sends the same result');
-$t->is($values['params']['query'], 'query', 'include_search_result() passes the query');
-
-include_search_controls($foo);
-$values = include_partial(null, null, true);
-$t->is($values['partial'], 'sfLucene/controls', 'include_search_controls() selects the correct partial');
-$t->ok($values['params']['form'] === $foo, 'include_search_controls() sends the same form');
-
-include_search_pager($foo, $bar, 8);
-$values = include_partial(null, null, true);
-$t->is($values['partial'], 'sfLucene/pagerNavigation', 'include_search_pager() selects the correct partial');
-$t->ok($values['params']['pager'] === $foo, 'include_search_pager() sends the same pager');
-$t->ok($values['params']['form'] === $bar, 'include_search_pager() sends the same form');
-$t->is($values['params']['radius'], 8, 'include_search_pager() sends the correct radius');
 
 $t->diag('testing highlighting');
 
