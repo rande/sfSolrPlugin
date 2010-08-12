a3f6980f68fea772838fd9f9cadcf70201c6fa48
Bertrand Zuchuat <bertrand.zuchuat@gmail.com>
2010-08-12 23:52:33 +0200

    Default version of xml on constructor

--- lib/highlighter/sfLuceneHighlighterXML.class.php
+++ lib/highlighter/sfLuceneHighlighterXML.class.php
@@ -29,13 +29,8 @@ class sfLuceneHighlighterXML extends sfLuceneHighlighter
    * @param string $version The XML version of the string
    * @param string $encoding The encoding of the string
    */
-  public function __construct($data, $version = null, $encoding = null)
+  public function __construct($data, $version = '1.0', $encoding = null)
   {
-    if (is_null($version))
-    {
-      $version = '1.0';
-    }
-    
     if (is_null($encoding))
     {
       $encoding = sfConfig::get('sf_charset', 'utf-8');
